bfbe32d3ac99ada4821dcdf8b47daa933426a09c
devMooon <anstjdus0702@duksung.ac.kr>
2022-02-17 18:01:36 +0900

    visibility

--- AndroidStudioProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidStudioProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -34,5 +36,10 @@ class MainActivity : AppCompatActivity() {
 
         //화면 출력 XML 명시
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.visible)
+        button.setOnClickListener{
+            button.visibility = View.INVISIBLE
+        }
     }
 }
