65b2fd5789dc059f1d4079eb1e603ac6723c8f5d
devMooon <anstjdus0702@duksung.ac.kr>
2022-02-18 14:20:14 +0900

    ch6 실습

--- AndroidStudioProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidStudioProjects/My Application/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,42 +4,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /*//이름 문자열 출력 TextView 생성
-        val name = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise"
-        }
-        //이미지 출력 ImageView 생성
-        val image = ImageView(this).also {
-            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.bird))
-        }
-        //주소 문자열 출력 TextView 생성
-        val address = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise, AB, 캐나다"
-        }
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            //LinearLayout 객체에 TextView, ImageView, TextView 객체 추가
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
-        //LinearLayout 객체를 화면에 출력
-        setContentView(layout)*/
-
         //화면 출력 XML 명시
         setContentView(R.layout.activity_main)
 
-        val button : Button = findViewById(R.id.visible)
-        button.setOnClickListener{
-            button.visibility = View.INVISIBLE
+        val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.visible.setOnClickListener{
+            binding.image.visibility = View.VISIBLE
+            //binding.image.setVisibility(View.VISIBLE)
+        }
+        binding.invisible.setOnClickListener{
+            binding.image.visibility = View.INVISIBLE
         }
     }
 }
